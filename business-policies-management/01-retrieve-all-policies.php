7cdc83d2eebae65e91ecd17d2dc0cf3cbe38a4fd
David T. Sadler <davidtsadler@googlemail.com>
2014-04-22 15:56:55 +0100

    Add first business policies management example.

---
+++ business-policies-management/01-retrieve-all-policies.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * Copyright 2014 David T. Sadler
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Include the SDK by using the autoloader from Composer.
+ */
+require __DIR__.'/../vendor/autoload.php';
+
+/**
+ * Include the configuration values.
+ *
+ * Ensure that you have edited the configuration.php file 
+ * to include your application keys.
+ * 
+ * For more information about getting your application keys, see:
+ * http://devbay.net/sdk/guides/application_keys.html
+ */
+$config = require __DIR__.'/../configuration.php';
+
+/**
+ * The namespaces provided by the SDK.
+ */
+use \DTS\eBaySDK\HttpClient;
+use \DTS\eBaySDK\Constants;
+use \DTS\eBaySDK\BusinessPoliciesManagement\Services;
+use \DTS\eBaySDK\BusinessPoliciesManagement\Types;
+
+/**
+ * Create the service object.
+ *
+ * For more information about creating a service object, see:
+ * http://devbay.net/sdk/guides/getting_started.html#service-object
+ *
+ * Note that an user token is required when using the Business Policies Management service. 
+ *
+ * For more information about getting your user tokens, see:
+ * http://devbay.net/sdk/guides/application_keys.html
+ */
+$service = new Services\BusinessPoliciesManagementService(new HttpClient\HttpClient(), array(
+    'authToken' => $config['production']['userToken'],
+    'globalId' => Constants\GlobalIds::US
+));
+
+/**
+ * Create the request object.
+ *
+ * For more information about creating a request object, see:
+ * http://devbay.net/sdk/guides/getting_started.html#request-object
+ */
+$request = new Types\GetSellerProfilesRequest();
+
+/**
+ * Send the request to the getSellerProfiles service operation.
+ *
+ * For more information about calling a service operation, see:
+ * http://devbay.net/sdk/guides/getting_started.html#service-operation
+ */
+$response = $service->getSellerProfiles($request);
+
+/**
+ * Output the result of calling the service operation.
+ *
+ * For more information about working with the service response object, see:
+ * http://devbay.net/sdk/guides/getting_started.html#response-object
+ */
+if ($response->ack !== 'Success') {
+    if (isset($response->errorMessage)) {
+        foreach ($response->errorMessage->error as $error) {
+            printf("Error: %s\n", $error->message);
+        }
+    }
+} else {
+    /**
+     *  Have to take into account that a seller may not have any business policies.
+     *  When no policies exist the API does not return the appropriate field.
+     *  Using isset ensures that we don't try and access properties that haven't been set. 
+     */
+    if (isset($response->paymentProfileList)) {
+        echo "================\nPayment Profiles\n================\n";
+        foreach ($response->paymentProfileList->paymentProfile as $profile) {
+            printf("(%s) %s: %s\n",
+                $profile->profileId,
+                $profile->profileName,
+                $profile->profileDesc
+            );
+        }
+    }
+
+    if (isset($response->returnPolicyProfileList)) {
+        echo "======================\nReturn Policy Profiles\n======================\n";
+        foreach ($response->returnPolicyProfileList->returnPolicyProfile as $profile) {
+            printf("(%s) %s: %s\n",
+                $profile->profileId,
+                $profile->profileName,
+                $profile->profileDesc
+            );
+        }
+    }
+
+    if (isset($response->shippingPolicyProfile)) {
+        echo "========================\nShipping Policy Profiles\n========================\n";
+        foreach ($response->shippingPolicyProfile->shippingPolicyProfile as $profile) {
+            printf("(%s) %s: %s\n",
+                $profile->profileId,
+                $profile->profileName,
+                $profile->profileDesc
+            );
+        }
+    }
+}
---
+++ business-policies-management/README.md
@@ -0,0 +1,7 @@
+# Business Policies Management Examples
+
+These examples show how to use the eBay SDK for PHP with the Business Policies Management service.
+
+1. [Retrieve all policies](https://github.com/davidtsadler/ebay-sdk-examples/blob/master/business-policies-management/01-retrieve-all-policies.php)
+
+   A basic example that retrieves all the Business Policies for a seller and displays some basic information about them. 
