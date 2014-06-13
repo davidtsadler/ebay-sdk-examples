7faf6b0b5afba048966a561d7a764440c0d8c64e
David T. Sadler <davidtsadler@googlemail.com>
2014-06-13 13:04:14 +0100

    Add new trading example.

---
+++ trading/02-get-category-hierarchy.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * Copyright 2014 David T. Sadler
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Include the SDK by using the autoloader from Composer.
+ */
+require __DIR__.'/../vendor/autoload.php';
+
+/**
+ * Include the configuration values.
+ *
+ * Ensure that you have edited the configuration.php file
+ * to include your application keys.
+ *
+ * For more information about getting your application keys, see:
+ * http://devbay.net/sdk/guides/application-keys/
+ */
+$config = require __DIR__.'/../configuration.php';
+
+/**
+ * The namespaces provided by the SDK.
+ */
+use \DTS\eBaySDK\Constants;
+use \DTS\eBaySDK\Trading\Services;
+use \DTS\eBaySDK\Trading\Types;
+
+/**
+ * Create the service object.
+ *
+ * For more information about creating a service object, see:
+ * http://devbay.net/sdk/guides/getting-started/#service-object
+ */
+$service = new Services\TradingService(array(
+    'apiVersion' => $config['tradingApiVersion'],
+    'siteId' => Constants\SiteIds::US
+));
+
+/**
+ * Create the request object.
+ *
+ * For more information about creating a request object, see:
+ * http://devbay.net/sdk/guides/getting-started/#request-object
+ */
+$request = new Types\GetCategoriesRequestType();
+
+/**
+ * An user token is required when using the Trading service.
+ *
+ * For more information about getting your user tokens, see:
+ * http://devbay.net/sdk/guides/application-keys/
+ */
+$request->RequesterCredentials = new Types\CustomSecurityHeaderType();
+$request->RequesterCredentials->eBayAuthToken = $config['production']['userToken'];
+
+/**
+ * By specifying 'ReturnAll' we are telling the API return the full category hierarchy.
+ */
+$request->DetailLevel = array('ReturnAll');
+
+/**
+ * OutputSelector can be used to reduce the amount of data returned by the API.
+ * http://developer.ebay.com/DevZone/XML/docs/Reference/ebay/GetCategories.html#Request.OutputSelector
+ */
+$request->OutputSelector = array(
+    'CategoryArray.Category.CategoryID',
+    'CategoryArray.Category.CategoryParentID',
+    'CategoryArray.Category.CategoryLevel',
+    'CategoryArray.Category.CategoryName'
+);
+
+/**
+ * Send the request to the GetCategories service operation.
+ *
+ * For more information about calling a service operation, see:
+ * http://devbay.net/sdk/guides/getting-started/#service-operation
+ */
+$response = $service->getCategories($request);
+
+/**
+ * Output the result of calling the service operation.
+ *
+ * For more information about working with the service response object, see:
+ * http://devbay.net/sdk/guides/getting-started/#response-object
+ */
+if ($response->Ack !== 'Success') {
+    if (isset($response->Errors)) {
+        foreach ($response->Errors as $error) {
+            printf("Error: %s\n", $error->ShortMessage);
+        }
+    }
+} else {
+    /**
+     * For the US site this will output approximately 18,000 categories.      
+     */
+    foreach ($response->CategoryArray->Category as $category) {
+        printf("Level %s : %s (%s) : Parent ID %s\n", 
+            $category->CategoryLevel,
+            $category->CategoryName,
+            $category->CategoryID,
+            $category->CategoryParentID[0]
+        );
+    }
+}
--- trading/README.md
+++ trading/README.md
@@ -5,3 +5,7 @@ These examples show how to use the eBay SDK for PHP with the Trading service.
 1. [Get eBay Official Time](https://github.com/davidtsadler/ebay-sdk-examples/blob/master/trading/01-get-ebay-official-time.php)
 
    A basic example that retrieves the official eBay system time in GMT. This is a good way of testing your production eBay authorization tokens as the call does not modify any user data.
+
+1. [Get Category Hierarchy](https://github.com/davidtsadler/ebay-sdk-examples/blob/master/trading/02-get-category-hierarchy.php)
+
+   Shows how to retrieve the category hierarchy for a site. More information can be found in the [official eBay documentation](http://developer.ebay.com/DevZone/guides/ebayfeatures/Development/Categories-Hierarchy.html).
